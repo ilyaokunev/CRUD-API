6604c4697b78601546273d36ddf363122c767f20
Ilya Okunev <zhoramc1@gmail.com>
2024-10-21 19:12:05 +0300

    feat: add prod mode

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,17 +4,17 @@ import { validate } from "uuid";
 import { IRequestUserDto } from "interfaces/dto.js";
 
 class UserController {
-
   constructor() {
     this.createUser = this.createUser.bind(this);
     this.updateUser = this.updateUser.bind(this);
   }
-  
+
   async getAllUsers(_: http.IncomingMessage, res: http.ServerResponse): Promise<void> {
     const users = await db.getAllUsers();
     res.writeHead(200);
 
     res.end(JSON.stringify(users));
+    return;
   }
 
   async getUser(
@@ -27,15 +27,18 @@ class UserController {
     if (!validate(id)) {
       res.writeHead(400);
       res.end("Invalid id type");
+      return;
     } else {
       const user = await db.getUser(id);
 
       if (user) {
         res.writeHead(200);
         res.end(JSON.stringify(user));
+        return;
       } else {
         res.writeHead(404);
         res.end("User not exist");
+        return;
       }
     }
   }
@@ -60,7 +63,7 @@ class UserController {
 
         const user = id ? await db.updateUser(userData, id) : await db.createUser(userData);
 
-        res.writeHead(id ? 200: 201);
+        res.writeHead(id ? 200 : 201);
         res.end(JSON.stringify(user));
         return;
       } else {
@@ -75,34 +78,49 @@ class UserController {
   }
 
   async updateUser(_: http.IncomingMessage, res: http.ServerResponse, query: { id: string }) {
+    try {
+      if (!validate(query.id)) {
+        res.writeHead(400);
+        res.end("Invalid id type");
+        return;
+      }
 
-    if (!validate(query.id)) {
-      res.writeHead(400);
-      res.end("Invalid id type");
-    }
-
-    if (!db.hasUser(query.id)) {
-      res.writeHead(404);
-      res.end('Invalid id');
-    } else {
-      await this.createUser(_, res, query.id);
+      if (!db.hasUser(query.id)) {
+        res.writeHead(404);
+        res.end("Invalid id");
+        return;
+      } else {
+        await this.createUser(_, res, query.id);
+      }
+    } catch (e) {
+      res.writeHead(500);
+      res.end(e);
+      return;
     }
   }
 
-  async deleteUser(_: http.IncomingMessage, res: http.ServerResponse, query: { id: string }) {    
+  async deleteUser(_: http.IncomingMessage, res: http.ServerResponse, query: { id: string }) {
+    try {
+      if (!validate(query.id)) {
+        res.writeHead(400);
+        res.end("Invalid id type");
+        return;
+      }
 
-    if (!validate(query.id)) {
-      res.writeHead(400);
-      res.end("Invalid id type");
-    }
-
-    if (!db.hasUser(query.id)) {
-      res.writeHead(404);
-      res.end('Invalid id');
-    } else {
-      res.writeHead(204);
-      await db.deleteUser(query.id);
-      res.end();
+      if (!db.hasUser(query.id)) {
+        res.writeHead(404);
+        res.end("Invalid id");
+        return;
+      } else {
+        res.writeHead(204);
+        await db.deleteUser(query.id);
+        res.end();
+        return;
+      }
+    } catch (e) {
+      res.writeHead(500);
+      res.end(e);
+      return;
     }
   }
 
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
 import http from "node:http";
 import url from "node:url";
-import router from './routes.js';
-import dotenv from 'dotenv';
+import router from "./routes.js";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
 const server = http.createServer(async (req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  const path = parsedUrl.pathname;
-  const method = req.method.toUpperCase();
-  const query = path.startsWith('/api/users/') ? {id:path.replace('/api/users/', '')} : null
-  const pathForHandler =  path.startsWith('/api/users/') ? '/api/users/:id' : path
-  const handler = router[pathForHandler] && router[pathForHandler][method];
+  try {
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const method = req.method.toUpperCase();
+    const query = path.startsWith("/api/users/") ? { id: path.replace("/api/users/", "") } : null;
+    const pathForHandler = path.startsWith("/api/users/") ? "/api/users/:id" : path;
+    const handler = router[pathForHandler] && router[pathForHandler][method];
 
-  if (handler) {
-    handler(req, res, query);
-  } else {
-    router.notFound(req, res);
+    if (handler) {
+      handler(req, res, query);
+    } else {
+      router.notFound(req, res);
+    }
+  } catch (e) {
+    res.writeHead(500);
+    res.end(e);
   }
-  
 });
 
-server.listen(PORT,() => console.log(`\n\nserver starts on port ${PORT}`));
+server.listen(PORT, () => console.log(`\n\nserver starts on port ${PORT}`));
